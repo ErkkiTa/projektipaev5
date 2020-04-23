bea99ed22da9b3452c776da90203c78ee29ccfd7
ErkkiTa <erkkitamb5@gmail.com>
2020-04-23 15:23:14 +0300

    Tere2 git

--- klaviatuur.js
+++ klaviatuur.js
@@ -1,9 +1,19 @@
+
+const answerInput = document.querySelector("#answer");
+
+
+answerInput.addEventListener("keydown", keyHandler);
+
 function keyHandler(event) {
     event = event || window.event;
-    if(event.ctrlKey && event.altKey && event.keyCode==67 ) {
-        // CTRL + ALT + C on vajutatud, popib roheline valgus ette korra
+    if(event.ctrlKey && event.keyCode==67 ) {
+        // CTRL + C on vajutatud, popib roheline valgus ette korra
     }
     else {
         // kui ei ole vajutatud seda siis ta viskab punase valguse korra ette
     }
 }
+
+
+
+ 
